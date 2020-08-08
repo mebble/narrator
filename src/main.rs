a75da54703c16bdcdde60cedc96bc15ad3c67aab
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-08-08 21:35:47 +0530

    print and sleep code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time};
+use std::io::{self, stdout, Write};
+
+fn main() -> io::Result<()> {
+    let duration = time::Duration::from_millis(3000);
+    let now = time::Instant::now();
+
+    print!("Gonna sleep now, world...\n");
+    stdout().flush()?;
+
+    thread::sleep(duration);
+    assert!(now.elapsed() >= duration);
+
+    print!("Good morning, world!\n");
+
+    Ok(())
 }
